16ee2b2b1f8030eba69abf8298f14e300ff2ba53
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-10-30 16:52:53 +0000

    halfway through implementing hit points, errors

--- battle_2/spec/player_spec.rb
+++ battle_2/spec/player_spec.rb
@@ -2,6 +2,7 @@ require 'player'
 
 describe Player do
   subject(:dave) { Player.new('Dave') }
+  subject(:mittens) { Player.new('Mittens') }
 
   describe '#name' do
     it 'returns the name' do
@@ -9,4 +10,22 @@ describe Player do
     end
   end
 
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(mittens).to receive(:receive_damange)
+      dave.attack(mittens)
+    end
+  end
+
+  describe '#receive_damange' do
+    it 'reduces the player hit points' do
+      expect { dave.receive_damange }.to change { dave.hit_points }.by(-10)
+    end
+  end
 end
