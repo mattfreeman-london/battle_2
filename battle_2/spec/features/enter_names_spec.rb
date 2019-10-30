0043d564494f37a3b4ee4b50242633d8bcaea2f4
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-10-30 14:35:14 +0000

    got everything working again

--- battle_2/spec/features/enter_names_spec.rb
+++ battle_2/spec/features/enter_names_spec.rb
@@ -1,9 +1,10 @@
-feature 'entering names' do
-  scenario 'can enter their names and see them on screen' do
+
+feature 'Enter names' do
+  scenario 'submitting names' do
     visit('/')
     fill_in :player_1_name, with: 'Dave'
     fill_in :player_2_name, with: 'Mittens'
     click_button 'Submit'
-    expect(page).to have_content 'Dave vs Mittens'
+    expect(page).to have_content 'Dave vs. Mittens'
   end
 end
