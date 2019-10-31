599b811fbd305070a1e5d3fda9b988b59bedeaf3
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-10-31 16:37:17 +0000

    refactored

--- battle_2/spec/game_spec.rb
+++ battle_2/spec/game_spec.rb
@@ -1,32 +1,32 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new('Dave', 'Mittens') }
-  let(:dave) { double :player }
-  let(:mittens) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
 
   describe '#player1' do
     it 'retrieves the first player' do
-      expect(game.player_1.name).to eq 'Dave'
+      expect(game.player_1).to eq player_1
     end
   end
 
   describe '#player2' do
     it 'retrieves the first player' do
-      expect(game.player_2.name).to eq 'Mittens'
+      expect(game.player_2).to eq player_2
     end
   end
 
   context 'start game' do
     it 'starts a game with one player' do
-      expect(game.player_1.name).to eq 'Dave'
+      expect(game.player_1).to eq player_1
     end
   end
 
   describe '#attack' do
     it 'damages the player' do
-      expect(mittens).to receive(:receive_damange)
-      game.attack(mittens)
+      expect(player_2).to receive(:receive_damange)
+      game.attack(player_2)
     end
   end
 end
