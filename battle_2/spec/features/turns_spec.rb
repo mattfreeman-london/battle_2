de4d3fde535746cbdd2363b065ce1c8511a265c0
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-11-01 14:55:59 +0000

    fixed changing turn problem

--- battle_2/spec/features/turns_spec.rb
+++ battle_2/spec/features/turns_spec.rb
@@ -6,8 +6,8 @@ feature 'taking turns' do
 
   scenario 'after player 1 attacks' do
     sign_in_and_play
-    click_button 'Attack'
+    click_button 'Attack Mittens'
     expect(page).not_to have_content "Dave's turn"
-    expect(page).to have_content "Mitten's turn"
+    expect(page).to have_content "New Turn: Mittens"
   end
 end
