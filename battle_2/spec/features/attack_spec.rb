1202b194b146fcab512160aeae452bd946e06c9a
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-11-01 14:40:49 +0000

    turn order is the last problem left

--- battle_2/spec/features/attack_spec.rb
+++ battle_2/spec/features/attack_spec.rb
@@ -1,13 +1,13 @@
 feature 'attacking' do
   scenario 'hitting player 2' do
     sign_in_and_play
-    attack_and_ok
+    click_button('Attack')
     expect(page).to have_content 'Dave attacked Mittens'
   end
 
   scenario 'reduce player2 HP by 10' do
     sign_in_and_play
-    attack_and_ok
+    click_button('Attack')
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
