210a4b581e52939a1f2d63261c40ee548f355516
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-11-01 12:17:13 +0000

    six syntax errors, forgot to commit earlier

--- battle_2/app.rb
+++ battle_2/app.rb
@@ -26,7 +26,7 @@ class Battle < Sinatra::Base
   get '/attack' do
     @game = $game
     @game.attack(@game.player_2)
-    @gane.switch_turns
+    @game.switch_turns
     erb :attack
   end
 
--- battle_2/spec/features/turns_spec.rb
+++ battle_2/spec/features/turns_spec.rb
@@ -1,15 +1,13 @@
-describe 'taking turns' do
-  context 'seeing the current turn' do
-    scenario 'at the start of the game' do
-      sign_in_and_play
-      expect(page).to have_content "Dave's turn"
-    end
+feature 'taking turns' do
+  scenario 'at the start of the game' do
+    sign_in_and_play
+    expect(page).to have_content "Dave's turn"
+  end
 
-    scenario 'after player 1 attacks' do
-      sign_in_and_play
-      click_button 'Attack'
-      expect(page).not_to have_content "Dave's turn"
-      expect(page).not_to have_content "Mitten's turn"
-    end
+  scenario 'after player 1 attacks' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).not_to have_content "Dave's turn"
+    expect(page).to have_content "Mitten's turn"
   end
 end
